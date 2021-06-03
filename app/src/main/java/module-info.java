d126c747494c9dfe9e2020ac7277380946b2228a
Daniel <igbodani14@gmail.com>
2021-06-03 12:51:49 -0600

    gson

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module Calculator.app.main {
     requires javafx.controls;
     requires  javafx.graphics;
+    requires com.google.common;
+    requires com.google.gson;
+    opens edu.isu.cs.student;
+    exports edu.isu.cs.student;
 }
